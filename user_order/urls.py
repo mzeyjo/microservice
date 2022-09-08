8e10e87c9668389f202c6eb3e3a42cafb592e64a
EC2 Default User <ec2-user@ip-172-31-18-94.ap-northeast-2.compute.internal>
2022-09-08 14:26:30 +0000

    first

--- user_order/urls.py
+++ user_order/urls.py
@@ -2,14 +2,23 @@ from django.urls import path
 
 from .views import ShopViewSet, OrderViewSet
 
-urlspatterns = [
+urlpatterns = [
     path('shop', ShopViewSet.as_view({
         'get':'list',
         'post':'create'
     })),
-    path('shop', ShopViewSet.as_view({
+    path('shop/<str:pk>', ShopViewSet.as_view({
         'get':'retrieve',
         'put':'update',
         'delete':'destroy',
     })),
+    path('order', OrderViewSet.as_view({
+        'get':'list',
+        'post':'create'
+    })),
+    path('order/<str:pk>', OrderViewSet.as_view({
+        'get':'retrieve',
+        'put':'update',
+        'delete':'destroy',
+    }))
 ]
